bdb02d9cccaaeb5413316efc82f365e979500c74
Aminat Amoo <aminaamoo@gmail.com>
2019-02-19 14:32:33 +0000

    reboot

--- app/api.py
+++ app/api.py
@@ -15,6 +15,28 @@ def getdb():
     c=conn.cursor()
     return (c,conn)
 
+def create_table():
+    c,conn=getdb()
+    c.execute('CREATE TABLE IF NOT EXISTS tasks (id REAL, title TEXT, date DATE, time TIME, description TEXT, priority TEXT, status TEXT)')
+ 
+def data_entry(id_t,title,date,time,description,priority,status):
+    c,conn=getdb()
+    c.execute("INSERT INTO tasks VALUES(?,?,?,?,?,?,?)",(id_t,title,date,time,description,priority,status))
+    conn.commit() 
+    c.close() 
+    conn.close() 
+    
+def delete_task(id_t):
+    c,conn=getdb()
+    c.execute('DELETE FROM tasks WHERE id=?',(id_t,))
+    conn.commit()
+    
+def edit_task(id_t,title,date,time,description,priority,status):
+    c,conn=getdb()
+    c.execute('DELETE FROM tasks WHERE id=?',(id_t,))
+    data_entry(id_t,title,date,time,description,priority,status)
+    conn.commit()
+    
 def retrieve_data():
     c,conn=getdb()
     c.execute('SELECT * FROM tasks')
