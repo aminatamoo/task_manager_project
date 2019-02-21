dd9fc3c8d4802da92d46fd013fa2767b6dacc051
Ottilie Williams <ottiliewilliams1@sky.com>
2019-02-21 14:54:01 +0000

    wip

--- app/mock.py
+++ app/mock.py
@@ -4,39 +4,95 @@ Created on Tue Feb 19 16:02:26 2019
 
 @author: ottil
 """
-
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, flash, redirect, url_for
 from flask_cors import CORS
+from api import *
+import requests
 
 app = Flask("MyApp")
 cors = CORS(app)
+app.config['SECRET_KEY']='reds209ndsldssdsljdslddbfudfbidabdfnfsfis'
 
-
-@app.route("/")
+#    
+##-----------------HTML TEMPLATE--------------#
+@app.route("/index",methods=['GET','POST'])
 def index():
-    data={
-            'Monday': [{'id': 1, 'title': 'Homework', 'date': '2019-02-18', 'day': 'Monday', 'time': '11:30:00', 'description': 'Do python chapter 1', 'priority': 'high', 'status': 'complete'}], 
-            'Tuesday': [], 
-            'Wednesday': [{'id': 2, 'title': 'Dinner', 'date': '2019-02-20', 'day': 'Wednesday', 'time': '11:30:00', 'description': 'Make dinner', 'priority': 'medium', 'status': 'incomplete'}], 
-            'Thursday': [{'id': 3, 'title': 'Clean', 'date': '2019-02-21', 'day': 'Thursday', 'time': '10:15:00', 'description': 'Clean kitchen and living room', 'priority': 'low', 'status': 'incomplete'}], 
-            'Friday': [], 
-            'Saturday': [], 
-            'Sunday': [{'id': 7, 'title': 'Sleep', 'date': '2019-02-21', 'day': 'Thursday', 'time': '10:15:00', 'description': 'Clean kitchen and living room', 'priority': 'low', 'status': 'incomplete'}], 
-            'high': [{'id': 1, 'title': 'Homework', 'date': '2019-02-18', 'day': 'Monday', 'time': '11:30:00', 'description': 'Do python chapter 1', 'priority': 'high', 'status': 'complete'}], 
-            'due': [{'id': 2, 'title': 'Dinner', 'date': '2019-02-20', 'day': 'Wednesday', 'time': '11:30:00', 'description': 'Make dinner', 'priority': 'medium', 'status': 'incomplete'}]}
-    return render_template("index.html", data=data)
-  
-@app.route("/viewtask")
-def view_task():
-    task={'Monday': [{'id': 1, 'title': 'Homework', 'date': '2019-02-18', 'day': 'Monday', 'time': '11:30:00', 'description': 'Do python chapter 1', 'priority': 'high', 'status': 'complete'}], 'Tuesday': [], 'Wednesday': [{'id': 2, 'title': 'Dinner', 'date': '2019-02-20', 'day': 'Wednesday', 'time': '11:30:00', 'description': 'Make dinner', 'priority': 'medium', 'status': 'incomplete'}], 'Thursday': [{'id': 3, 'title': 'Clean', 'date': '2019-02-21', 'day': 'Thursday', 'time': '10:15:00', 'description': 'Clean kitchen and living room', 'priority': 'low', 'status': 'incomplete'}], 'Friday': [], 'Saturday': [], 'Sunday': [], 'high': [{'id': 1, 'title': 'Homework', 'date': '2019-02-18', 'day': 'Monday', 'time': '11:30:00', 'description': 'Do python chapter 1', 'priority': 'high', 'status': 'complete'}], 'due': [{'id': 2, 'title': 'Dinner', 'date': '2019-02-20', 'day': 'Wednesday', 'time': '11:30:00', 'description': 'Make dinner', 'priority': 'medium', 'status': 'incomplete'}]}
-    return render_template("view_task.html", task=task)
+    payload='alltasks'
+    data=call_api_alltask(payload)
+    if request.method=='POST':
+        status =request.form.getlist('check')
+        status_process(status,data)
+        return redirect(url_for('index'))            
+    if request.method=='GET':
+        return render_template('index.html',data=data)
 
+@app.route("/showtask/<id_t>",methods=['GET','POST'])
+def showtask(id_t):
+    payload='task/{}'.format(id_t)
+    data=call_api_onetask(payload)
+    id_t=id_t
+    title=data['tasks'][0]['title']
+    date=data['tasks'][0]['date']
+    time=data['tasks'][0]['time']
+    description=data['tasks'][0]['description']
+    priority=data['tasks'][0]['priority']
+    status=data['tasks'][0]['status']
+    if request.method=='POST':
+        if request.form['edit_button']=='Edit Task':
+            return redirect(url_for('edittask', id_t=id_t))
+        if request.form['delete_button']=='Delete Task':
+            delete_task(id_t)
+            return redirect(url_for('index'))
+        if request.form['close_button']=='Close Task':
+            return redirect(url_for('index'))
+            
+    if request.method=='GET':
+        return render_template('showtask.html',**locals())
 
-@app.route("/api")
-def index_api():
-    data={'Monday': [{'id': 1, 'title': 'Homework', 'date': '2019-02-18', 'day': 'Monday', 'time': '11:30:00', 'description': 'Do python chapter 1', 'priority': 'high', 'status': 'complete'}], 'Tuesday': [], 'Wednesday': [{'id': 2, 'title': 'Dinner', 'date': '2019-02-20', 'day': 'Wednesday', 'time': '11:30:00', 'description': 'Make dinner', 'priority': 'medium', 'status': 'incomplete'}], 'Thursday': [{'id': 3, 'title': 'Clean', 'date': '2019-02-21', 'day': 'Thursday', 'time': '10:15:00', 'description': 'Clean kitchen and living room', 'priority': 'low', 'status': 'incomplete'}], 'Friday': [], 'Saturday': [], 'Sunday': [], 'high': [{'id': 1, 'title': 'Homework', 'date': '2019-02-18', 'day': 'Monday', 'time': '11:30:00', 'description': 'Do python chapter 1', 'priority': 'high', 'status': 'complete'}], 'due': [{'id': 2, 'title': 'Dinner', 'date': '2019-02-20', 'day': 'Wednesday', 'time': '11:30:00', 'description': 'Make dinner', 'priority': 'medium', 'status': 'incomplete'}]}
-    data=jsonify(data)
+@app.route("/edittask/<id_t>",methods=['GET','POST'])
+def edittask(id_t):
+    payload='task/{}'.format(id_t)
+    data=call_api_onetask(payload)
+    id_t=id_t
+    title=data['tasks'][0]['title']
+    date=data['tasks'][0]['date']
+    time=data['tasks'][0]['time']
+    description=data['tasks'][0]['description']
+    priority=data['tasks'][0]['priority']
+    status=data['tasks'][0]['status']
+    if request.method=='POST':
+        pass      
+    if request.method=='GET':
+        return render_template('edittask.html',**locals()) 
+    
+@app.route("/addtask",methods=['GET','POST'])
+def addtask():
+    if request.method=='POST':
+
+        title=request.form['title']
+        date=request.form['date']
+        time=request.form['time']
+        priority=request.form['priority']
+        description=request.form['description']
+        data_entry(title, date, time, priority, description)
+        flash('You\'ve successfully added a task', 'success')       
+        return redirect(url_for('index'))
+    if request.method=='GET':
+        return render_template('addtask.html',**locals())
+ 
+#----------------API URLS--------------------#
+ 
+@app.route("/alltasks")
+def alltasks():
+    data = retrieve_all_tasks()
     return data
+        
+@app.route("/task/<int:id_t>")
+def task_id(id_t):
+    data = retrieve_a_task(id_t) 
+    return data
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
